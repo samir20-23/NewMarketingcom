876d542e41c035ae4b688ac697a25034025b0a85
samir <germanysamir1@gmail.com>
2024-06-27 23:40:57 +0100

    whassap and option

---
+++ MARKETINGCOM/backend/config/connect.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = 'root';
+$db = "marketingcom";
+$password = "";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {                  
+  echo "Connection failed: " . $e->getMessage();
+}
