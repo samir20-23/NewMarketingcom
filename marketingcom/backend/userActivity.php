41b9c99f4ce85102d6446cf38db692efce10ec5e
samir aoulad amar <144660881+samir20-23@users.noreply.github.com>
2024-06-27 16:38:44 +0100

    Add files via upload

---
+++ marketingcom/backend/userActivity.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'config/connect.php';
+
+if (isset($_POST['command']) && !empty($_POST['phone_number'])) {
+    // if phone number pushed means new user so this code creates new user
+    $sql = 'SELECT user_phone FROM `user` WHERE user_phone = ' . $_POST['phone_number'];
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if (empty($result["user_phone"])) {
+        if (isset($_POST['phone_number'])) {
+            if (empty($_POST['phone_number']) || strlen($_POST['phone_number']) < 10 || strlen($_POST['phone_number']) > 17) {
+                die(json_encode("numberinvald"));
+            }
+            if (empty($_POST['user_name']) || strlen($_POST['user_name']) < 5) {
+                die(json_encode("nameinvalid"));
+            } else {
+                $sql = 'INSERT INTO user (`user_id`, `user_name`, `user_phone`) VALUES (NULL, " ' . $_POST['user_name'] . '", "' . $_POST['phone_number'] . '")';
+                $stmt = $conn->prepare($sql);
+                $stmt->execute();
+            }
+        }
+    }
+    // after created new user if needed a command is created in db
+    $sql = 'INSERT INTO commander (user_id, service_id, date)
+    VALUES ((SELECT user_id FROM user WHERE user_phone = "' . $_POST['phone_number'] . '"), ' . $_POST['service_id'] . ', NOW());';
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    echo json_encode("verified");
+} else {
+    echo json_encode("phoneempty");
+}
---
+++ marketingcom/frontend/marketing.js
@@ -0,0 +1,30 @@
+let services = document.getElementById("services");
+
+let request = new XMLHttpRequest();
+request.open("GET", "../backend/marketing.php?services");
+request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+request.send();
+request.onload = () => {
+  let response = JSON.parse(request.response);
+  console.log(response);
+  response.forEach((item) => {
+    services.innerHTML += `<div class="service" name="${item.service_name}" id="${item.service_id}">
+            <img src="${item.service_img}" class="icons" />
+            <h3  class="service-title">${item.service_name}</h3>
+          </div>`;
+  });
+
+  let service = document.querySelectorAll(".service");
+
+  service.forEach((element) => {
+    element.addEventListener("click", () => {
+      
+      const id = element.id;
+      const name = element.getAttribute('name');
+      const url = `services.html?id=${id}&name=${name}`;
+      window.location = url;
+    });
+  });
+};
+
+//
