2020b80888fa1de597f61e7bf9f0a6d5f278d947
Abderrafie Elmrabet <abderafieelmrabet@gmail.com>
2024-06-28 09:49:03 -0700

    whatsapp

--- MARKETINGCOM/frontend/script/serviceDetails.js
+++ MARKETINGCOM/frontend/script/serviceDetails.js
@@ -163,6 +163,7 @@ xhr.onload = function () {
         }
         selected_options.value = commandOptions;
         //whatsapp send
+        let message = JSON.parse(selected_options.value);
         let mynumber = "+2120718087106";
         whatsapp.addEventListener("click", function () {
           reload.style.display = "flex";
