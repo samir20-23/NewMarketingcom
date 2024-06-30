15d111d5b25cc2b8f6fc537846248757fc71d228
aymen <devaymen15.5.3@gmail.com>
2024-06-30 23:01:29 +0100

    added login page for admin

---
+++ MARKETINGCOM/admin/index.php
@@ -0,0 +1,24 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/index.page.css">
+    <title>Login</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <form class="form-signin">
+            <h2 class="form-signin-heading">Please login</h2>
+            <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
+            <input type="password" class="form-control" name="password" placeholder="Password" required="" /><br>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+        </form>
+    </div>
+</body>
+
+</html>
