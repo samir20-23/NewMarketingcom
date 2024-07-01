3768fa7c1d1097dff6565ccaf5d03d2cbad9e0c2
aymen <devaymen15.5.3@gmail.com>
2024-07-01 20:14:35 +0100

    completed login for admin

--- MARKETINGCOM/admin/index.php
+++ MARKETINGCOM/admin/index.php
@@ -1,4 +1,30 @@
 <?php
+require_once('../backend/config/connect.php');
+session_start();
+$error = '';
+
+if (isset($_SESSION['admin'])) {
+    navigateToAdminPage();
+}
+
+if (isset($_POST['admin_login'])) {
+    $sql = 'SELECT * FROM `admin` WHERE `email`= "'.$_POST['email'].'"';
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
+    if (empty($result)) {
+        $error = 'Invalid Data';
+    }elseif($result['password'] === $_POST['password']){
+        echo'<script>sessionStorage.setItem("admin", "true");</script>';
+        $_SESSION['admin'] = 'true';
+        navigateToAdminPage();
+    }
+    
+}
+
+function navigateToAdminPage(){
+    header("Location: adminPage.php");
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,11 +38,12 @@
 
 <body>
     <div class="wrapper">
-        <form class="form-signin">
+        <form class="form-signin" method="POST">
             <h2 class="form-signin-heading">Please login</h2>
-            <input type="text" class="form-control" name="username" placeholder="Email Address" required="" autofocus="" />
+            <input type="text" class="form-control" name="email" placeholder="Email Address" required="" autofocus="" />
             <input type="password" class="form-control" name="password" placeholder="Password" required="" /><br>
-            <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+            <p><?=$error?></p>
+            <input class="" name="admin_login" type="submit">
         </form>
     </div>
 </body>
