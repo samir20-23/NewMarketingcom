b3dad2af5c7fd8ed50be8f26ca3c843f03de22c6
Ayoub <elbouzidiayoub.solicode@gmail.com>
2024-07-03 04:08:21 +0200

    payment

---
+++ MARKETINGCOM/backend/process_payment.php
@@ -0,0 +1,39 @@
+<?php
+require_once 'git autoload.php';
+
+\Stripe\Stripe::setApiKey('your_stripe_secret_key');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $card_number = $_POST['card_number'];
+    $exp_date = $_POST['exp_date'];
+    $cvv = $_POST['cvv'];
+    $card_name = $_POST['card_name'];
+
+    // Split expiration date into month and year
+    $exp_month = substr($exp_date, 0, 2);
+    $exp_year = substr($exp_date, 3, 2);
+
+    try {
+        $token = \Stripe\Token::create([
+            'card' => [
+                'number' => $card_number,
+                'exp_month' => $exp_month,
+                'exp_year' => $exp_year,
+                'cvc' => $cvv,
+                'name' => $card_name,
+            ],
+        ]);
+
+        $charge = \Stripe\Charge::create([
+            'amount' => 1000, // Amount in cents (e.g., $10.00)
+            'currency' => 'usd',
+            'source' => $token->id,
+            'description' => 'Example charge',
+        ]);
+
+        echo 'Payment successful!';
+    } catch (\Stripe\Exception\CardException $e) {
+        echo 'Error: ' . $e->getError()->message;
+    }
+}
+?>
